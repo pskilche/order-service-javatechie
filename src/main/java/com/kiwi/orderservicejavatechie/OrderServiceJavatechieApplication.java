5fa613d5dc2266c8ec909c1d58f061e4477d0ef6
pkilche <pkilche@ebates.com>
2024-04-13 14:27:01 +0530

    Initial commit

---
+++ src/main/java/com/kiwi/orderservicejavatechie/OrderServiceJavatechieApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.orderservicejavatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceJavatechieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceJavatechieApplication.class, args);
+	}
+
+}
